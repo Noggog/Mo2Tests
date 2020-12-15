1bd304014f31a72002acf3dd7e1e1bc71b18ec84
Justin Swanson <justin.c.swanson@gmail.com>
2020-12-15 17:07:19 -0600

    Initial test

---
+++ Delay/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Delay
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(15000);
+        }
+    }
+}
---
+++ Mo2Tests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mo2Tests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //System.Console.WriteLine("Started single subprocess");
+            //var process = Process.Start("Delay.exe");
+            //await Task.Delay(2000);
+            //process.Kill();
+            //process.WaitForExit();
+            //System.Console.WriteLine("Killed single subprocess");
+            //System.Console.WriteLine("Press enter to continue");
+            //System.Console.ReadLine();
+
+            System.Console.WriteLine("Started subprocesses");
+            await Task.WhenAll(Enumerable.Range(0, 25).Select(_ => Task.Run(async () =>
+            {
+                var process = Process.Start("Delay.exe");
+                await Task.Delay(2000);
+                process.Kill();
+                process.WaitForExit();
+            })));
+            System.Console.WriteLine("Killed subprocesses");
+            System.Console.WriteLine("Press enter to exit");
+            System.Console.ReadLine();
+        }
+    }
+}
